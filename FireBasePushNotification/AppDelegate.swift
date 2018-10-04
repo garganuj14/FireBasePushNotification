0c806e264c03ede3d5f0439033d98263db2ca298
Anuj garg <garganuj14@gmail.com>
2018-10-04 12:47:49 +0530

    commit by anuj

--- FireBasePushNotification/AppDelegate.swift
+++ FireBasePushNotification/AppDelegate.swift
@@ -108,7 +108,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
         
         // TODO: If necessary send token to application server.
-        SendData_API(url: self.url, fcm_ID: self.fcm_token)
+      //  SendData_API(url: self.url, fcm_ID: self.fcm_token)
 
         // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
